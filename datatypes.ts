65e376b2571de22f37c5b53e3fde04e2b530f957
rutvi pandya <64300471+rutvi779@users.noreply.github.com>
2021-12-13 03:10:08 +0530

    Create datatypes.ts

---
+++ datatypes.ts
@@ -0,0 +1,22 @@
+let a:number=10
+let b:string="hello"
+let c:boolean=true
+let d:undefined=undefined
+let e:null=null
+let mobile:Object={
+    brand:"apple",
+    color:"red",
+    price:35000
+}
+let colors:string[]=['white','black','silver']
+let greet = (name:string):void=>{
+
+}
+enum Month{
+    JAN='JANUARY',
+    FEB='FEBRUARY'
+}
+
+let r:any='test';
+r=10
+a=12
