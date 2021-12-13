c1e71d6adfd557e0366b4471a606eb82e60152c1
rutvi pandya <64300471+rutvi779@users.noreply.github.com>
2021-12-13 22:50:57 +0530

    Create object.ts

---
+++ object.ts
@@ -0,0 +1,25 @@
+interface Address{
+    street:string;
+    city:string;
+    state:string;
+}
+interface Student{
+    brand:string;
+    color:string;
+    price:number;
+    address:Address
+
+}
+
+let student:Student={
+
+    brand:"apple",
+    color:"pink",
+    price:34000,
+    address:{
+        street:"hfgkgml",
+        city:"fjn",
+        state:"fgnjhg"
+    }
+
+}
